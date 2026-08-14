47218e3750fa5cf75e4380517b853ee17fb22d0d
Kalyan Gaund <081bei015@ioepc.edu.np>
2026-08-14 23:04:08 +0545

    Disk usage debug

---
+++ disk_error.sh
@@ -0,0 +1,18 @@
+## Debuggging if disk problem in server
+
+check_disk(){
+	echo
+	echo "Checking disk usage..."
+	
+	df -h /
+	if dfn -h / | awk 'NR==2 {gsub("%","",$5);
+		if ($5>90) exit 1;
+		else
+			exit 0}'; then
+			echo "Disk usage is normal"
+		else
+			echo "Disk usage is too high"
+			echo "DEBUG: The disk may be full"
+	fi
+}
+
