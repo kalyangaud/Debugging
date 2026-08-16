ade2efb2633272d0c5548e847931dbda3004d740
Kalyan Gaund <081bei015@ioepc.edu.np>
2026-08-16 22:15:49 +0545

    CPU usage debug modified

--- CPU.sh
+++ CPU.sh
@@ -3,8 +3,16 @@
 echo "Checking server status..."
 
 CPU=$(top -bn1 | grep Cpu | awk '{print $2}')
- if
-	 ["$CPU" -gt 80 ];then
-	 echo "Warning :CPU uasge is too high"
- fi
+ 
+echo "CPU usage: $CPU%"
+
+if awk "BEGIN {exit !($CPU >80)};
+then
+	echo "Warning:CPU usage is too high."
+fi
+
+TOP_PROCESS=$(ps -eo pid,comm,%cpu --sort=-%cpu | head -n 2 | tail -n 1)
+echo "Top CPU consuming process:"
+echo "$TOP_PROCESS"
+
  echo "Done"
